564d3cca3f6e9eea51c074255fd023a416829dbd
Henwuar <henrypullan@gmail.com>
2021-08-30 15:48:28 +0100

    Tweaked Generic IK implementation to use weight rather than lerps

--- Runtime/GenericIKController.cs
+++ Runtime/GenericIKController.cs
@@ -4,17 +4,22 @@ namespace BLBits
 {
     public class GenericIKController : MonoBehaviour
     {
+        [Range(0.0f,1.0f)]
+        public float weight;
         public Transform headBone;
         public Transform lookTarget;
         public Vector3 turnAxis;
         public Vector3 tiltAxis;
         public Vector3 lookOffset = Vector3.zero;
 
+        public bool automateWeight = false;
+        public float weightAutomationSpeed = 1.0f;
+
         public float maxHeadTurn = 30.0f;
         public float maxHeadTilt = 15.0f;
 
-        public float headTurnLerpSpeed = 2.5f;
-        public float headTurnLerpSpeedIgnoreState = 10.0f;
+        //public float headTurnLerpSpeed = 2.5f;
+        //public float headTurnLerpSpeedIgnoreState = 10.0f;
 
         public bool objectUsesAnimation = true;
 
@@ -23,6 +28,8 @@ namespace BLBits
 
         private Quaternion targetRotation = Quaternion.identity;
         private Quaternion startHeadRotation;
+
+        private float targetWeight = 0;
 
         protected void Start()
         {
@@ -38,10 +45,17 @@ namespace BLBits
             {
                 targetAnimator = GetComponent<Animator>();
             }
+
+            targetWeight = weight;
         }
 
         void LateUpdate()
         {
+            if(automateWeight && targetWeight != weight)
+            {
+                weight = Mathf.MoveTowards(weight, targetWeight, weightAutomationSpeed * Time.deltaTime);
+            }
+
             if (headBone)
             {
                 bool shouldIgnore = false;
@@ -58,9 +72,18 @@ namespace BLBits
                         if (targetAnimator.GetCurrentAnimatorStateInfo(0).IsName(ignoreStates[i]) || targetAnimator.GetNextAnimatorStateInfo(0).IsName(ignoreStates[i]))
                         {
                             shouldIgnore = true;
+                            if(automateWeight)
+                            {
+                                targetWeight = 0;
+                            }
                             break;
                         }
                     }
+                }
+
+                if(automateWeight && shouldIgnore == false)
+                {
+                    targetWeight = lookTarget != null ? 1 : 0;
                 }
 
                 if (lookTarget != null && shouldIgnore == false)
@@ -79,10 +102,9 @@ namespace BLBits
                     Quaternion targetTurn = Quaternion.AngleAxis(turnAngle, turnAxis);
                     Quaternion targetTilt = Quaternion.AngleAxis(tiltAngle, tiltAxis);
 
-                    targetRotation = Quaternion.Lerp(targetRotation, headBone.rotation * targetTilt * targetTurn, headTurnLerpSpeed * Time.deltaTime);
-                    headBone.rotation = targetRotation;
+                    targetRotation = headBone.rotation * targetTilt * targetTurn;
                 }
-                else if (shouldIgnore)
+                /*else if (shouldIgnore)
                 {
                     targetRotation = Quaternion.Lerp(targetRotation, headBone.rotation, headTurnLerpSpeedIgnoreState * Time.deltaTime);
                     Quaternion baseRotation = objectUsesAnimation ? headBone.rotation : startHeadRotation;
@@ -96,13 +118,19 @@ namespace BLBits
                 {
                     targetRotation = Quaternion.Lerp(targetRotation, headBone.rotation, headTurnLerpSpeed * Time.deltaTime);
                     headBone.rotation = targetRotation;
-                }
+                }*/
+
+                headBone.rotation = Quaternion.Lerp(headBone.rotation, targetRotation, weight);
             }
         }
 
         public void SetLookTarget(Transform target)
         {
             lookTarget = target;
+            if(automateWeight)
+            {
+                targetWeight = lookTarget != null ? 1 : 0;
+            }
         }
 
         public void SnapToTarget()
@@ -143,10 +171,12 @@ namespace BLBits
             if (headBone)
             {
                 Gizmos.color = Color.green;
-                Gizmos.DrawLine(headBone.position, headBone.position + headBone.TransformDirection(turnAxis).normalized * 2.5f);
+                Vector3 turn = headBone.TransformDirection(turnAxis).normalized;
+                Gizmos.DrawLine(headBone.position, headBone.position + turn * 2.5f);
 
                 Gizmos.color = Color.red;
-                Gizmos.DrawLine(headBone.position, headBone.position + headBone.TransformDirection(tiltAxis).normalized * 2.5f);
+                Vector3 tilt = headBone.TransformDirection(tiltAxis).normalized;
+                Gizmos.DrawLine(headBone.position, headBone.position + tilt * 2.5f);
 
                 Gizmos.color = Color.blue;
                 Vector3 forward = Vector3.Cross(headBone.TransformDirection(tiltAxis), headBone.TransformDirection(turnAxis)).normalized;
